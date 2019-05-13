c4577b834ef2eeaf5dd74865a929bdc17a5e84b4
bolajiadeyemi <d.bola1@gmail.com>
2019-05-13 05:33:32 +0100

    Add components

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe [] = [new Recipe('A test Recipe', 'This is simply a test',
+    'https://www.telegraph.co.uk/content/dam' +
+    '/food-and-drink/2019/05/10/fish_trans_NvBQzQNjv4BqEDjTm7JpzhSGR1_8ApEWQA1vLvhkMtVb21dMmpQBfEs.jpg?imwidth=1400')];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
